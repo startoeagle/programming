d6fba44262618a27ef213b8d4c2d6a326279e76c
Anton Karlsson <anton93karlsson@gmail.com>
2018-09-13 08:37:37 +0200

    saftey commit

--- missingnumber/main.c
+++ missingnumber/main.c
@@ -22,13 +22,22 @@ int main(){
   }
   unsigned int len = i;
   int missing_number = missing(arr, len);
-  printf("The missing number is %d\n", missing_number);
+  if (missing_number)
+    printf("The missing number is %d\n", missing_number);
+  else
+    printf("There are no missing numbers\n");
   return 0;
 }
 
 int missing(int arr[], unsigned int len){
+  printf("(");
+  for(int i=0; i<len; i++){
+    printf("%d ", arr[i]);
+  }
+  printf(")");
+  printf("\n");
   if (len == 1)
-    return 0; // There are no missing integers in this subarray
+    return arr[0];
   else if (len == 2)
     return arr[1] - arr[0] > 1 ? arr[1] - 1 : 0;
   else{
@@ -36,7 +45,18 @@ int missing(int arr[], unsigned int len){
     int arr_new[len/2];
     for (int i = len/2; i<len; i++)
       arr_new[i-len/2] = arr[i];
-    return missing(arr, len - len/2) + missing(arr_new, len/2);
+
+    int m1 = missing(arr, len - len/2);
+    printf("m1 = %d\n", m1);
+    printf("m2 = %d\n", m2);
+    int m2 = missing(arr_new, len/2);
+    if(len/2 == 1)
+      // int this case we need to check if the missing number
+      // is between the two subarrays
+      return arr[len-len/2-1] - m2 > 1 ? m2 - 1 : 0 + m1;
+    else
+      return m1 + m2;
+	
   }
      
 }
