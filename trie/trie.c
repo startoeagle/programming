c4afff603ef37d34aa122f2531e5a9d3d5102c71
Anton Karlsson <anton93karlsson@gmail.com>
2018-09-17 09:15:06 +0200

    Now the removal of nodes functions.

--- trie/trie.c
+++ trie/trie.c
@@ -87,28 +87,82 @@ bool match(trie * t, char key[]){
   else
     return false;
 }
+
+bool is_prefix(trie * t, char key[]){
+  // returns true if the key key[] is a prefix to another key
+  // goto en d key
+  node * head = t->head;
+  int index;
+  for (int i = 0; i<strlen(key); i++){
+    index = get_index(key[i]);
+    head = head->children[index];
+  }
+  for (int j = 0; j<strlen(key); j++){
+    if (head->children[j] != NULL)
+      return true;
+  }
+
+  // when this code is reached no key countinen after argument key
+  return false;
+}
+
+bool contains_prefix(trie * t, char key[]){
+  // Check if the argument char * contains any end_key=true
+  node * head = t->head;
+  int index;
+  for(int i = 0; i<strlen(key)-1; i++){
+    index = get_index(key[i]);
+    if(head->children[index]->end_key == true)
+      return true;
+    head = head->children[index];
+  }
+  return false;
+}
+
 void node_remove(trie * t, char key[]){
   // check if key exist in directory
   node * current  = t->head;
   t->nr_words--;
-  int i,j, index;
+  int i, index;
   int len = strlen(key);
-  bool have_children = false;
-  if(match(t, key)){
-
-    for (i=0; i<len; i++){
+  if(match(t, key) && !is_prefix(t, key) && !contains_prefix(t, key)){
+    // delete all nodes in key if there are no prefixes the key is a
+    // part of, or there are no prefixes of the key that also is a
+    // key.
+    index = get_index(key[0]);
+    node_free(current->children[index]);
+    free(current->children[index]);
+    current->children[index] = NULL;
+  
+  }
+  else if (match(t, key) && is_prefix(t, key)){
+    // the argument key is a prefix of another key, then unmark the leaf node
+    for (i = 0; i<len; i++){
       index = get_index(key[i]);
-      // if node is end of key[]  set end key true for its parrent
-      if(current->end_key && i == len-1){
-	printf("Hello!\n");
-        
-	current->end_key=false;
-	return;
-      }
-      else {
-	current = current->children[index];
-      }	  
+      current=current->children[index];
     }
+    current->end_key = false;
+  }
+  else if (match(t, key) && contains_prefix(t, key)){
+    // If the key cotains a prefix, then delete it after the longest
+    // prefix in the argument key ends.
+    int longest_prefix = 0;
+    for (i = 0; i<len; i++){
+      index = get_index(key[i]);
+      if (current->children[index]->end_key)
+	longest_prefix = i;
+      current = current->children[index];
+    }
+    // go back to the longets prefix
+    current = t->head;
+    for(i = 0; i<longest_prefix; i++){
+      index = get_index(key[i]);
+      current = current->children[index];
+    }
+    // remove the nodes from this point
+    node_free(current);
+    free(current);
+    current = NULL;
   }
   else
     printf("The key does not exist. Nothing is removed\n");
