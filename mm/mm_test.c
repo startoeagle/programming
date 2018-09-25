1bcba98b2ec5e4a040d8f61cb6ae961df10c56bf
Anton Karlsson <anton93karlsson@gmail.com>
2018-09-25 12:34:44 +0200

    Made test program for gsl_cblas

--- mm/mm_test.c
+++ mm/mm_test.c
@@ -1,44 +1,53 @@
 #include <stdio.h>
+#include <malloc.h>
 #include <gsl/gsl_cblas.h>
-#include <lapacke.h>
-
+// #include <lapacke.h>
+void print(double * a, int row, int col){
+   for(int i = 0; i<row; i++){
+    printf("[");
+    for(int j = 0; j<col-1; j++)
+      printf("%f ", a[i*col + j]);
+    printf("%f]\n", a[i*col + col-1]);
+  }
+   printf("\n");
+}
+void printa(double * a, int len){
+  for(int i = 0; i<len; i++){
+    printf("[");
+    printf("%f]\n", a[i]);
+  }
+  printf("\n");
+}
 int main(){
   double a[2][2] = {{7,2},{2,2}};
   double b[2][2] = {{-7,2},{4,-2}};
   int m,n,k,lda,ldb,ldc;
   m=n=k=lda=ldb=ldc = 2;
   double out[2][2] = {0};
-
-  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+  printf("Multiplying\n");
+  print(*a, 2, 2);
+  printf("with\n");
+  print(*b, 2, 2);
+  
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
 			 m, n, k, 1, *a, lda, *b, ldb, 0, *out, ldc);
+  print(*out, m, n);
 
-  for(int i = 0; i<m; i++){
-    printf("[");
-    for(int j = 0; j<n-1; j++)
-      printf("%f ", out[i][j]);
-    printf("%f]\n", out[i][n-1]);
-  }
+  // multply a matrix with a vector
 
-  
-  // try to to solve a linear sys of equations using lapacke
-  // a[2][2] = {{7,2},{2,2}};
-  double b_new[2][1] = {{-7},{-2}};
-  int nrhs, *ipiv;
-  ipiv = (int *) malloc(sizeof(int)*m);
-  
-  n=k=lda=ldb=ldc = 2;
-  nrhs = 2;
-  
-  int info = LAPACKE_dgesv(LAPACK_COL_MAJOR, n, nrhs, *a, lda, ipiv, *b_new, ldb);
-  if (info)
-    printf("Something went wrong\n");
-
-  for(int i = 0; i<m; i++){
-    printf("[");
-    for(int j = 0; j<n-1; j++)
-      printf("%f ", b_new[i][j]);
-    printf("%f]\n", b_new[i][n-1]);
-  }
-
+  /* double *x = (double *) malloc(2*sizeof(double)); */
+  /* x[0] = 1; */
+  /* x[1] = 3; */
+  double x[2]= {2,1};
+  double *y = (double *) malloc(2*sizeof(double));
+  printf("Multiplying\n");
+  print(*a, 2, 2);
+  printf("with\n");
+  printa(x, 2);
+  cblas_dgemv(CblasColMajor, CblasNoTrans,
+	      m, n, 1.0, *a, lda, x, 1, 0.0, y, 1);
+  printa(y, n);
+  /* free(x); */
+  free(y);
   
 }
